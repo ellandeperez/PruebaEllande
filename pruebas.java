3e44646e6bf7c0d35c5c8c67d12c76f94693bc02
Ellande <ellandeperez@gmail.com>
2018-02-26 14:01:25 +0100

    1

---
+++ pruebas.java
@@ -0,0 +1,28 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class pruebas
+{
+     
+    private int x;
+
+    /**
+     * Constructor  
+     */
+    public pruebas()
+    {
+         
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
